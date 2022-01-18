66ca4026fbc8f4dd44818206cc8579b56a70f97e
Squidman95 <squidmanse@gmail.com>
2022-01-18 11:44:15 +0100

    Update CreateReportSteps.java

--- ReportService/src/test/java/dtu/services/CreateReportSteps.java
+++ ReportService/src/test/java/dtu/services/CreateReportSteps.java
@@ -1,30 +1,19 @@
 package dtu.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.mock;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.UUID;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 import dtu.ReportService.Application.ReportService;
 import dtu.ReportService.Domain.Payment;
-import dtu.ReportService.Domain.Token;
+import dtu.ReportService.Domain.PaymentMerchant;
 import dtu.ReportService.Infrastructure.ReportRepository;
-//import dtu.TokenService.Presentation.Resources.ReportMessageService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-//import messaging.MessageQueue;
-import io.restassured.internal.path.json.JSONAssertion;
 
 public class CreateReportSteps {
 	ReportService reportService = new ReportService(new ReportRepository());
-	String report, customer1, customer2, customer3, merchant1, merchant2, merchant3, token1, token2, token3, amount1, amount2, amount3;
+	HashSet<Payment> report;
+	HashSet<PaymentMerchant> merchantReport;
+	String customer1, customer2, customer3, merchant1, merchant2, merchant3, token1, token2, token3, amount1, amount2, amount3;
 
 	@Given("a report exists")
 	public void aReportExists() {
@@ -55,7 +44,7 @@ public class CreateReportSteps {
 
 	@When("a merchant with id {string} requests a full report")
 	public void aMerchantWithIdRequestsAFullReport(String merchantId) {
-		report = reportService.getMerchantReport(merchantId);
+		merchantReport = reportService.getMerchantReport(merchantId);
 	}
 
 	@When("a customer with id {string} requests a full report")
@@ -65,30 +54,6 @@ public class CreateReportSteps {
 	
 	@Then("the entire report is returned")
 	public void theEntireReportIsReturned() {
-		JsonObject expectedReport = new JsonObject();
-		JsonElement paymentJson;
-//		paymentJson.
-//		expectedReport.add(token1, expectedReport);
 	}
-	
-//	@Given("a customer with id {string}")
-//	public void aCustomerWithId(String customerId) {
-//		this.customerId = customerId;
-//	}
-//
-//	@Given("the customer already has {int} tokens")
-//	public void theCustomerAlreadyHasTokens(Integer numOfTokens) {
-//		tokens = reportService.createTokens(numOfTokens, customerId);
-//	}
-//
-//	@When("the customer requests {int} tokens")
-//	public void theCustomerRequestsTokens(Integer numOfTokens) {
-//		tokens = reportService.createTokens(numOfTokens, customerId);
-//	}
-//
-//	@Then("the customer has {int} tokens")
-//	public void theCustomerHasTokens(Integer expectedNumOfTokens) {
-//		assertEquals(expectedNumOfTokens, tokens.size());
-//	}
 
 }
